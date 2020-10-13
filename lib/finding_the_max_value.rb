6bba69b2f75ec4eeb92598b89eddeaa7252dd43f
June Young Kang <jkang34@gmail.com>
2020-10-13 21:06:34 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,5 +1,11 @@
 def find_max_value(array)
   count = 0 
+  max_value = -1
   while count < array.length do 
-    if 
+    if max_value < array[count]
+      max_value = array[count]
+    end 
+    count += 1 
+  end
+  max_value
 end
